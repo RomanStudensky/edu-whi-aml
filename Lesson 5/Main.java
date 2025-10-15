00fb76eec570b7854ab3a32a04bd667c9c03cd76
WhiAmI <glebpenza97@gmail.com>
2025-10-15 18:06:08 +0400

    Add Directory Lesson 5 Add Main,Programmer,Driver Add Interface Workable

---
+++ Lesson 5/Main.java
@@ -0,0 +1,28 @@
+interface Workable {
+    void work();
+}
+
+  class Programmer implements Workable {
+      public void work() {
+          System.out.println("Программист пишет код");
+      }
+  }
+
+  class Driver implements Workable {
+      public void work(){
+          System.out.println("Водитель ведет машину");
+      }
+  }
+
+  public class Main{
+      public static void main(String[] args) {
+          Workable[] work = new Workable[2];
+
+          work[0] = new Programmer();
+          work[1] = new Driver();
+
+          for(Workable w : work){
+              w.work();
+          }
+      }
+  }
